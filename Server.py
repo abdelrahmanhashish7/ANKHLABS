5febbb13203972976d2d19eb81c240ec967ef2fa
abdelrahmanhashish7 <138692892+abdelrahmanhashish7@users.noreply.github.com>
2025-12-03 13:51:29 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -335,7 +335,8 @@ def glucose_history():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=8000, debug=True, threaded=True)
+
 
 
 
